2a1fa1b48fbfd3d8fe69e04e240b84cde6e0f9d4
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-08 19:08:47 +0530

    Create Basic Input Output.cpp

---
+++ Basic I-O/Basic Input Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "\nYour name is: " << name;
+
+    return 0;
+}
