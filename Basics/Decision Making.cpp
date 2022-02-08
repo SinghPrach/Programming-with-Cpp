13b57cfefa9e657b0481e9fae398154f5e30ee83
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-08 13:09:00 +0530

    Create Decision Making.cpp

---
+++ Basics/Decision Making.cpp
@@ -0,0 +1,31 @@
+//if 
+#include<iostream>
+using namespace std;
+ 
+    int main()
+    {
+        int i = 10;
+  
+        if (i > 15)
+        {
+           cout<<"10 is less than 15";
+        }   
+        
+        cout<<"Outside of If";
+    }
+
+//if-else
+#include<iostream>
+using namespace std;
+ 
+int main()
+ {
+        int i = 20;
+  
+        if (i < 15)
+            cout<<"i is smaller than 15";
+        else
+            cout<<"i is greater than 15";
+             
+    return 0;   
+ }
