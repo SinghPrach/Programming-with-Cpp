4bc60b278bf4833e054f56b0965f44b54b72688a
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-08 13:09:43 +0530

    Update Decision Making.cpp

--- Basics/Decision Making.cpp
+++ Basics/Decision Making.cpp
@@ -29,3 +29,20 @@ int main()
              
     return 0;   
  }
+//if else ladder
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int i = 20;
+  
+    if (i == 10)
+        cout<<"i is 10";
+    else if (i == 15)
+        cout<<"i is 15";
+    else if (i == 20)
+        cout<<"i is 20";
+    else
+        cout<<"i is not present";
+}
