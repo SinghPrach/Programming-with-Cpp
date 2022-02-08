5ffdb490c34b8a95b490c0755062a6ebf3c98b14
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-08 19:25:15 +0530

    Update and rename file to Operators in C++.cpp

---
+++ Operators/Operators in C++.cpp
@@ -0,0 +1,43 @@
+//1. Arithmetic Operators: 
+//Unary Operators
+int val = 5;
+++val;  // 6
+
+//Binary Operators(*,/,-,+)
+int a = 7;
+int b = 2;
+cout<<a+b; // 9
+
+//2. Relational Operators
+int a = 3;
+int b = 5;
+a < b; // operator to check if a is smaller than b
+
+//3. Logical Operators
+//AND = &&
+//NOT equal to = !=
+
+//4. Bitwise Operators
+int a = 5, b = 9;   // a = 5(00000101), b = 9(00001001)
+cout << (a ^ b);   //  00001100
+cout <<(~a);       // 11111010
+
+//5. Assignment Operators
+//“=”
+a = 10;
+b = 20;
+ch = 'y';
+
+//“+=”
+(a += b) can be written as (a = a + b)
+//If initially value stored in a is 5. Then (a += 6) = 11.
+  
+//“-=”
+(a -= b) can be written as (a = a - b)
+//If initially value stored in a is 8. Then (a -= 6) = 2.  
+//“*=”
+(a *= b) can be written as (a = a * b)
+//If initially, the value stored in a is 5. Then (a *= 6) = 30.  
+//“/=”
+(a /= b) can be written as (a = a / b)
+// If initially, the value stored in a is 6. Then (a /= 2) = 3.  
