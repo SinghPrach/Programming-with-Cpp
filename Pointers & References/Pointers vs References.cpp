f58dfbb34e4f0a388eee821f64a8acde12660d0c
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-11 10:17:09 +0530

    Create Pointers vs References.cpp

---
+++ Pointers & References/Pointers vs References.cpp
@@ -0,0 +1,11 @@
+//A pointer is a variable that holds memory address of another variable. 
+
+//A reference variable is an alias or another name for an already existing variable. 
+
+int i = 3; 
+
+// A pointer to variable i or stores address of i
+int *ptr = &i; 
+
+// A reference ia an alias for i.
+int &ref = i; 
