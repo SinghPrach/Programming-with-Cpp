9aa67eba3d8decd81a0c0109062f4f50c5af5655
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-11 10:14:10 +0530

    Update Smart Pointers.cpp

--- Pointers & References/Smart Pointers.cpp
+++ Pointers & References/Smart Pointers.cpp
@@ -24,4 +24,43 @@ int main()
 
     return 0;
 }
-//
+//shared_ptr
+#include <iostream>
+using namespace std;
+#include <memory>
+
+class Rectangle {
+    int length;
+    int breadth;
+
+public:
+    Rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+
+    int area()
+    {
+        return length * breadth;
+    }
+};
+
+int main()
+{
+
+    shared_ptr<Rectangle> P1(new Rectangle(10, 5));
+
+    cout << P1->area() << endl;
+
+    shared_ptr<Rectangle> P2;
+    P2 = P1;
+
+    cout << P2->area() << endl;
+
+    cout << P1->area() << endl;
+
+    cout << P1.use_count() << endl;
+    return 0;
+}
+
